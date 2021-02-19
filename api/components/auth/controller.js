6bfb227063874b580bd2373076c1fba31c6ab0cc
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-02-19 17:57:16 -0400

    Encrypt password with bcrypt, added

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const auth = require('../../../auth');
 const TABLA = 'auth';
 
@@ -9,15 +10,19 @@ module.exports = function(injectedStore) {
 
     async function login(username, password) {
         const data = await store.query(TABLA, { username: username });
-        if (data.password === password) {
-            // Generar token;
-            return auth.sign(data);
-        } else {
-            throw new Error('Información inválida')
-        }
+
+        return bcrypt.compare(password, data.password)
+            .then(sonIguales => {
+                if (sonIguales === true) {
+                    // Generar token;
+                    return auth.sign(data);
+                } else {
+                    throw new Error('Información inválida');
+                }
+            });
     }
 
-    function upsert(data) {
+    async function upsert(data) {
         const authData = {
             id: data.id,
         }
@@ -27,7 +32,7 @@ module.exports = function(injectedStore) {
         }
 
         if (data.password) {
-            authData.password = data.password;
+            authData.password = await bcrypt.hash(data.password, 5);
         }
 
         return store.upsert(TABLA, authData);
